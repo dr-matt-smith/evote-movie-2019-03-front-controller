78a56f998b10aba4dfa0ea06ce57eafe2ae56348
matt smith <matt@matts-MacBook-Pro-2.local>
2019-02-21 07:38:28 +0000

    added Front Controller and public and templates directories

---
+++ templates/sitemap.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>EVOTE MOVIE - sitemap</title>
+    <meta charset="utf-8">
+    <style>
+        @import "/css/basic.css";
+        @import "/css/nav.css";
+        @import "/css/footer.css";
+    </style>
+</head>
+<body>
+
+<header>
+    <img src="/images/smithit_logo.gif" alt="logo">
+</header>
+
+
+<nav>
+    <ul>
+        <li>
+            <a href="/index.php">Home</a>
+        </li>
+
+        <li>
+            <a href="/index.php?action=about">About Us</a>
+        </li>
+
+        <li>
+            <a href="/index.php?action=list">Movie ratings</a>
+        </li>
+
+        <li>
+            <a href="/index.php?action=contact">Contact Us</a>
+        </li>
+
+        <li>
+            <a href="/index.php?action=sitemap" class="current_page">Site Map</a>
+        </li>
+    </ul>
+</nav>
+
+<h1>
+    Site Map
+</h1>
+
+<ul>
+    <li><a href="index">Home</a>
+    <li><a href="about">About us</a>
+    <li><a href="list">DVD ratings</a>
+    <li><a href="contact">Contact us</a>
+    <li><a href="sitemap">Site Map</a>
+</ul>
+
+
+<footer>
+    2019 &copy; A Matt Smith productions international enterprises limited production
+</footer>
+
+</body>
+</html>
